06b429bdf98f590b22aa75be43ecaca376fbd6c0
Al Amin Chayan <chayan.php@gmail.com>
2019-05-25 10:56:40 +0600

    supervisor update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\SimulateScore;
 use App\Jobs\StartMatch;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -28,6 +29,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->job(new StartMatch)->everyMinute();
+        $schedule->job(new SimulateScore())->everyMinute();
     }
 
     /**
